81390fc05064d393614ca19bde657736a625ce92
Mayank Lalwani <mayank.lalwani@gmail.com>
2021-06-17 14:48:36 +0530

    provider.tf updated

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
 provider "azurerm" {
-  version = ">=2.16.0"
   features {}
 }
