d321f4763775ac2f64da4f63b2e3218874d6c9ff
mayanklalwani <79749089+mayanklalwani@users.noreply.github.com>
2021-06-19 16:50:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=2.46.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name  = "terraform-state"
+    storage_account_name = "tfstoragetrainingml"
+    container_name       = "terraform-state-container"
+    key                  = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
